f45e3ae75270f729bc391a6103cbe012c6b0845e
RPi <RPi>
2017-04-06 12:29:36 +0000

    slengte på while og sleep

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from py532lib.i2c import *
 from py532lib.frame import *
 from py532lib.constants import *
+import time
  
 pn532 = Pn532_i2c()
 pn532.SAMconfigure()
  
-card_data = pn532.read_mifare().get_data()
- 
-print(card_data)
+
+while True:
+	card_data = pn532.read_mifare().get_data()
+	print(card_data)
+	time.sleep(5)
