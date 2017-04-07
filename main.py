e35758ee916bf73b489611184e6280cb8c62e79b
RPi <RPi>
2017-04-07 10:23:52 +0000

    Minor test of IR.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from py532lib.i2c import *
 from py532lib.frame import *
 from py532lib.constants import *
 import time
+import RPi.GPIO as GPIO
  
 pn532 = Pn532_i2c()
 pn532.SAMconfigure()
- 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.IN)
 
 while True:
 	card_data = pn532.read_mifare().get_data()
 	print(card_data)
+	#print(GPIO.input(22))
 	time.sleep(5)
