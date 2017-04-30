62186a0d1cf6e0d83e1efa3f392ce974b261d64c
RPi <RPi>
2017-04-30 11:41:44 +0000

    Tidy up some comments and import files. Re-added offline task.

--- tasks.py
+++ tasks.py
@@ -1,18 +1,22 @@
 import threading
+
 import time
 import paho.mqtt.client as mqtt
 import json
 
-import OldserverLiksom
+import software.OldserverLiksom as OldserverLiksom
 import GPIOhardware.hardware as hardware
 import GPIOhardware.rfid as rfid
 import GPIOhardware.berryclip as berryclip
+import software.mutexes as mutexes
 #import communication
-import mutexes
 
-t_LEDs = 2  ## LED timer; how long LED is active before turned off.
+
 
 roomId=1
+
+
+
 class myThread(threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
@@ -29,38 +33,31 @@ class myThread(threading.Thread):
         elif (self.threadID == 2):
             task_IR()
 
-        #		try :
-        #			if (self.threadID == 1):
-        #				task_RFIDandLED()
-        #			elif (self.threadID == 2):
-        #				task_IR()
-        #		except :
-        #			print("Thread exception!")
-        #		finally :
-        #			print("Thread " + self.name + " succesfully terminated")
+        print("Thread " + self.name + " is terminated.")
 
-        # print("Thread " + self.name + " is terminated.")
 
-def on_message(client, userdata, msg):
-    data = json.dumps(str(msg.payload))[2:-1]
-    print(data)
-    if data[-1]['type']=='cardAsked':
-        berryclip.setYellowLED(0)
-        if data[-1]['response']=='confirmed' or data[-1]['response']=='booked':
-            berryclip.setGreenLED(1)
-        if data[-1]['response']=='denied':
-            berryclip.setRedLED(1)
 
-def on_connect(client, userdata, flags, rc):
-    print(("connected with result code " + str(rc)))
-    client.subscribe("/fk/rr/2")
+#def on_message(client, userdata, msg):
+#    data = json.dumps(str(msg.payload))[2:-1]
+#    print(data)
+#    if data[-1]['type']=='cardAsked':
+#        berryclip.setYellowLED(0)
+#        if data[-1]['response']=='confirmed' or data[-1]['response']=='booked':
+#            berryclip.setGreenLED(1)
+#        if data[-1]['response']=='denied':
+#            berryclip.setRedLED(1)
+
+#def on_connect(client, userdata, flags, rc):
+#    print(("Connected with result code " + str(rc)))
+#    client.subscribe("/fk/rr/2")
+
 
 
 def runTasks():
     ## Setup and start extra threads.
-    # thread_rfid = myThread(1, "RFID control")
+    ## thread_rfid = myThread(1, "RFID control")
     # thread_ir = myThread(2, "IR control")
-    # thread_rfid.start()
+    ## thread_rfid.start()
     # thread_ir.start()
 
     ## Start main execution thread.
@@ -70,12 +67,19 @@ def runTasks():
 
 
 def task_noServRFIDandLED():
-    hardware.init()
-    task_period = 3  ## Timing for the LEDs, mainly.
-    while (True):
-	card_data = rfid.read()
-	OldserverLiksom.RFIDlookup(card_data)
+	hardware.init()
+	task_period = 3  ## Timing for the LEDs, mainly.
 	
+	while (True):
+		card_data = rfid.read()
+		user_access = OldserverLiksom.RFIDlookup(card_data) #Simulate server communication.
+		OldserverLiksom.roomUserAccess(user_access)
+		if (user_access):
+			mutexes.setIRactivation(1)
+			thread_ir = myThread(2, "IR control")
+			thread_ir.start()
+		time.sleep(task_period)
+		berryclip.resetRoomLEDs()
 
 
 def task_RFIDandLED():
@@ -97,7 +101,6 @@ def task_RFIDandLED():
             card_data=None
             time.sleep(2)
 
-'''	
 def task_IR ():
 	hardware.init()
 
@@ -116,7 +119,6 @@ def task_IR ():
 	mutexes.setIRactivation(0)
 	communication.resetIRtimer()
 
-	berryclip.setRedLED(1)
-	time.sleep(t_LEDs)
-	berryclip.resetRoomLEDs()
-'''
+	LEDs.blinkRed()
+
+	#print("IR task finished.")
