f190b1329d5a95eef927c2e54b5089695628acc0
RPi <RPi>
2017-04-30 13:35:46 +0000

    Extracted code and placed it in other files.

--- main.py
+++ main.py
@@ -1,70 +1,24 @@
 import RPi.GPIO as GPIO
 
 import time
-import paho.mqtt.client as mqtt
-import json
 
-import GPIOhardware.ir as ir
 import GPIOhardware.hardware as hardware
 import GPIOhardware.rfid as rfid
-
 import GPIOhardware.berryclip as berryclip
-import software.LEDs as LEDs
-
 from tasks import runTasks
+import communication.communication as communication
+import data.roomData as roomData
 
 
-
-roomId=1
 ## Main.
-
-
-
 hardware.init()
 
-
-
-def on_message(client, userdata, msg):
-        data = json.loads(msg.payload)
-        melding=data[0]
-        if melding['type']=='error':
-                print("error")
-                time.sleep(1)
-                berryclip.setYellowLED(0)
-        elif melding['type']=='cardAsked':
-                print("cardask")
-                berryclip.setYellowLED(0)
-                if melding['response']=='confirmed' or data[-1]['response']=='booked':
-                    berryclip.setGreenLED(1)
-                    time.sleep(2)
-                    berryclip.setGreenLED(0)
-                if melding['response']=='denied':
-                    berryclip.setRedLED(1)
-                    time.sleep(2)
-                    berryclip.setRedLED(0)
-
-def on_connect(client, userdata, flags, rc):
-    print(("Connected with result code " + str(rc)))
-    client.subscribe("/fk/rr/2")
-
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("iot.eclipse.org", 1883, 60)
-client.loop_start()
-irlast=0
-
-
-
 while (True):
-        card_data = rfid.read()
-        if card_data != None:
-                print(card_data.hex())
-                berryclip.setYellowLED(1)
-                print("got data")
-                data = {'roomId': roomId, 'RFID': card_data.hex(), 'command': 'cardask'}
-                client.publish('/fk/rr', json.dumps(data))
-                card_data=None
-                time.sleep(2)
+	card_data = rfid.read()
+	print("Got data")
+	berryclip.setYellowLED(1)
+	communication.send_message(roomData.roomID, card_data, 'cardask')
+	card_data=None
+	time.sleep(2)
+
 
