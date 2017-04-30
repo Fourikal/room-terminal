ab29ce08a3d2fe1ecd114e67b5601f23c17ca876
RPi <RPi>
2017-04-30 15:35:19 +0000

    Re-inserted ir. Moved code to tasks.py for multitasking.

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ import time
 
 import GPIOhardware.hardware as hardware
 import GPIOhardware.rfid as rfid
-import GPIOhardware.berryclip as berryclip
-from tasks import runTasks
 import communication.communication as communication
 import data.roomData as roomData
-
+import GPIOhardware.berryclip as berryclip
+from tasks import runTasks
+import software.mutexes as mutexes
 
 ## Main.
 hardware.init()
 
-while (True):
-	card_data = rfid.read()
-	print("Got data")
-	berryclip.setYellowLED(1)
-	communication.send_message(roomData.roomID, card_data, 'cardask')
-	card_data=None
-	time.sleep(2)
+try:
+#	while (True):
+#		card_data = rfid.read()
+#		print("Got data")
+#		communication.send_message(roomData.roomID, card_data, 'cardask')
+#		card_data=None
+#		time.sleep(2)
+#		mutexes.setIRactivation(1)
+#		runTasks()
+	runTasks()
+
+finally: 
+	print("Exiting. ")
+	hardware.close()
 
 
