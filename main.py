ed9b0780cf8c64a53590ecb91274c4c4fab7fa96
RPi <RPi>
2017-04-30 21:48:04 +0000

    May be working now. I think server went down, so I did not finish checking.

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import RPi.GPIO as GPIO
 import time
 
 import GPIOhardware.hardware as hardware
-import GPIOhardware.rfid as rfid
-import communication.communication as communication
-import data.roomData as roomData
-import GPIOhardware.berryclip as berryclip
-from tasks import runTasks
-import software.mutexes as mutexes
+#import GPIOhardware.rfid as rfid
+#import communication.communication as communication
+#import data.roomData as roomData
+#import GPIOhardware.berryclip as berryclip
+#from tasks import runTasks
+import software.tasks as tasks
+#import software.mutexes as mutexes
+#import communication.outgoing as outgoing
+
+
 
 ## Main.
+print("Main init. ")
 hardware.init()
 
 try:
@@ -18,14 +23,14 @@ try:
 #		card_data = rfid.read()
 #		print("Got data")
 #		communication.send_message(roomData.roomID, card_data, 'cardask')
+#		outgoing.send_message(roomData.roomID, card_data, 'cardask')
 #		card_data=None
 #		time.sleep(2)
 #		mutexes.setIRactivation(1)
 #		runTasks()
-	runTasks()
-
+#		tasks.runTasks()
+	tasks.runTasks()
 finally: 
 	print("Exiting. ")
 	hardware.close()
-
 
