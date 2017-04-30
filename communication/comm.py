e7b96598a847f6df1d981439b531823105015a1d
RPi <RPi>
2017-04-30 18:20:56 +0000

    Tried cleaning, but will go with this for now.

--- communication/comm.py
+++ communication/comm.py
@@ -4,6 +4,15 @@ import time
 
 import software.LEDs as LEDs
 import data.roomData as roomData
+import software.mutexes as mutexes
+#import software.behaviour as behaviour 
+import tasks
+
+
+
+client=mqtt.Client()
+client.loop_start()
+client.connect(roomData.CHANNEL_CONNECT, 1883, 60)
 
 
 
@@ -15,6 +24,8 @@ def on_message (client, userdata, msg):
 	data = json.loads(msg.payload)
 	melding = data[0]
 
+#	behaviour.receiveMessage(melding)
+
 	if melding['type'] == 'error':
 		print("Error")
 		time.sleep(1)
@@ -24,20 +35,37 @@ def on_message (client, userdata, msg):
 		LEDs.off()
 		if melding['response'] == 'confirmed' or data[-1]['response'] == 'booked':
 			LEDs.blinkGreen()
+			receiveUserAccept()
 		if melding['response'] == 'denied':
 			LEDs.blinkRed()
+			receiveUserReject()
+
 
 def send_message (roomID, card_data, commandType):
+	print("comm.send")
 	LEDs.setYellow(1)
 	data = {'roomId': roomID, 'RFID': card_data.hex(), 'command': commandType}
 	client.publish(roomData.CHANNEL_PUB, json.dumps(data))
 
 
 
-client=mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(roomData.CHANNEL_CONNECT, 1883, 60)
-client.loop_start()
+
+def receiveUserAccept ():
+        ## Verified booking or Instant booking was received.
+        LEDs.off()
+        LEDs.blinkGreen
+        mutexes.setIRactivation(1)
+        thread_ir = tasks.myThread(2, "IR control")
+        thread_ir.start()
+
+def receiveUserReject ():
+        ## User cannot book the room.
+        LEDs.off()
+        LEDs.blinkRed()
+
+
+
 
